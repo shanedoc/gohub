ba92aa7db52d08c0759c2bdbe635becae4f658ea
shanedoc <shanedoc@163.com>
2023-01-15 16:49:40 +0800

    key命令

--- main.go
+++ main.go
@@ -46,6 +46,7 @@ func main() {
 	// 注册子命令
 	rootCmd.AddCommand(
 		cmd.CmdServer,
+		cmd.CmdKey,
 	)
 	// 配置默认运行 Web 服务
 	cmd.RegisterDefaultCmd(rootCmd, cmd.CmdServer)
@@ -58,39 +59,3 @@ func main() {
 		console.Exit(fmt.Sprintf("Failed to run app with %v: %s", os.Args, err.Error()))
 	}
 }
-
-/*func main() {
-	//初始化gin
-	var env string
-
-	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
-	flag.Parse()
-	config.InitConfig(env)
-	//初始化logger
-	bootstrap.SetupLogger()
-
-	//设置gin运行模式,本地开发建议debug模式
-	gin.SetMode(gin.ReleaseMode)
-
-	// new 一个 Gin Engine 实例
-	router := gin.New()
-
-	//初始化db
-	bootstrap.SetupDB()
-
-	//初始化redis
-	bootstrap.SetupRedis()
-
-	// 初始化路由绑定
-	bootstrap.SetupRoute(router)
-
-	//verfiycode.NewVerifyCode().SendSMS("15652946160")
-
-	// 运行服务
-	err := router.Run(":" + config.Get("app.port"))
-	if err != nil {
-		// 错误处理，端口被占用了或者其他错误
-		fmt.Println(err.Error())
-	}
-
-}*/
