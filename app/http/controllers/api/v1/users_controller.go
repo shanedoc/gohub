e0622093edcbd1f0fb5bae3a7315ceaa217257a2
shanedoc <shanedoc@163.com>
2023-01-27 19:19:58 +0800

    验证分页氢气

--- app/http/controllers/api/v1/users_controller.go
+++ app/http/controllers/api/v1/users_controller.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/shanedoc/gohub/app/models/user"
+	"github.com/shanedoc/gohub/app/requests"
 	"github.com/shanedoc/gohub/pkg/auth"
 	"github.com/shanedoc/gohub/pkg/response"
 )
@@ -19,6 +20,15 @@ func (ctrl *UsersController) CurrentUser(c *gin.Context) {
 
 //index所有用户
 func (ctrl *UsersController) Index(c *gin.Context) {
+	/**data, pager := user.Paginate(c, 10)
+	response.JSON(c, gin.H{
+		"data":  data,
+		"pager": pager,
+	})**/
+	request := requests.PaginationRequest{}
+	if ok := requests.Validate(c, &request, requests.Pagination); !ok {
+		return
+	}
 	data, pager := user.Paginate(c, 10)
 	response.JSON(c, gin.H{
 		"data":  data,
