7d7c8c7615bbed0f3cd64cc5449577b9dd6ce420
shanedoc <shanedoc@163.com>
2023-01-30 17:02:10 +0800

    模版模型添加分页

--- app/http/controllers/api/v1/users_controller.go
+++ app/http/controllers/api/v1/users_controller.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/shanedoc/gohub/app/models/user"
 	"github.com/shanedoc/gohub/app/requests"
+
 	"github.com/shanedoc/gohub/pkg/auth"
 	"github.com/shanedoc/gohub/pkg/response"
 )
@@ -20,12 +21,8 @@ func (ctrl *UsersController) CurrentUser(c *gin.Context) {
 
 //index所有用户
 func (ctrl *UsersController) Index(c *gin.Context) {
-	/**data, pager := user.Paginate(c, 10)
-	response.JSON(c, gin.H{
-		"data":  data,
-		"pager": pager,
-	})**/
 	request := requests.PaginationRequest{}
+	//fmt.Println(request)
 	if ok := requests.Validate(c, &request, requests.Pagination); !ok {
 		return
 	}
